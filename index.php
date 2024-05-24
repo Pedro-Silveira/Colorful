de10ba362588403bae95c6ff0d15e68dc7d4100f
Pedro Silveira <pedro.silveiradev@gmail.com>
2024-05-24 14:03:36 -0300

    Initial files upload.

--- index.php
+++ index.php
@@ -1,34 +1,80 @@
 <?php
-
+# Imports
 require 'connection.php';
 
+# Variáveis
 $connection = new Connection();
-
 $users = $connection->query("SELECT * FROM users");
-
-echo "<table border='1'>
-
-    <tr>
-        <th>ID</th>    
-        <th>Nome</th>    
-        <th>Email</th>
-        <th>Ação</th>    
-    </tr>
-";
-
-foreach($users as $user) {
-
-    echo sprintf("<tr>
-                      <td>%s</td>
-                      <td>%s</td>
-                      <td>%s</td>
-                      <td>
-                           <a href='#'>Editar</a>
-                           <a href='#'>Excluir</a>
-                      </td>
-                   </tr>",
-        $user->id, $user->name, $user->email);
-
-}
-
-echo "</table>";
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página Inicial - Colorful</title>
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <script src="/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="/images/Colorful.png" alt="Logo" width="200" height="50" class="d-inline-block align-text-top">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown link
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <table border='1'>
+                <tr>
+                    <th>ID</th>    
+                    <th>Nome</th>    
+                    <th>Email</th>
+                    <th>Ação</th>    
+                </tr>
+                <?php
+                    foreach($users as $user){
+                        echo(
+                            "<tr>
+                                <td>" . $user->id . "</td>
+                                <td>" . $user->name . "</td>
+                                <td>" . $user->email . "</td>
+                                <td>
+                                    <a href='#'>Editar</a>
+                                    <a href='#'>Excluir</a>
+                                </td>
+                            </tr>"
+                        );
+                    };
+                ?>
+            </table>
+        </main> 
+    </body>
+</html>
